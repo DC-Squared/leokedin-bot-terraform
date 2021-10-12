aebf3e55b111767ad66d20a0769a05fcf048d230
Emerson Valentim <emevalentim0@gmail.com>
2021-10-12 11:41:30 -0300

    feat: add fargate spot on cluster definition

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -1,6 +1,9 @@
 resource "aws_ecs_cluster" "leokedin-ecs" {
   name = "leokedin-bot"
 
+  capacity_providers = [
+    "FARGATE_SPOT"
+  ]
   setting {
     name  = "containerInsights"
     value = "disabled"
